79c17dbe3c9b5cd10699dd4d6685ada3ff0b03b8
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-18 01:56:47 +0200

    Description concise des changements apportés

--- src/components/AffichageData.jsx
+++ src/components/AffichageData.jsx
@@ -14,8 +14,8 @@ const AffichageData = () => {
     return (
         <div className="bg-gray-200 flex flex-col">
             {error && <p>Error: {error.message}</p>}
-            <h1>Users</h1>
-            <ul className="flex flex-col">
+            <h1 className="text-center font-extrabold text-3xl">Users</h1>
+            <ul className="flex flex-col justify-center items-center py-2">
                 {users.map(user => (
                     <li key={user.id}>
                         {user.name} - {user.email}
