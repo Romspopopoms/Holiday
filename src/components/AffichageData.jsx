a664dee8a1501577dd7a4d5941b0e59d26cad022
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-18 02:01:23 +0200

    Description concise des changements apportés

--- src/components/AffichageData.jsx
+++ src/components/AffichageData.jsx
@@ -18,17 +18,17 @@ const AffichageData = () => {
             <table className="table-auto bg-white shadow-md rounded-lg w-3/4">
                 <thead className="bg-purple-400 text-white">
                     <tr>
-                        <th className="px-4 py-2">ID</th>
-                        <th className="px-4 py-2">Name</th>
-                        <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2 text-center">ID</th>
+                        <th className="px-4 py-2 text-center">Name</th>
+                        <th className="px-4 py-2 text-center">Email</th>
                     </tr>
                 </thead>
                 <tbody>
                     {users.map(user => (
                         <tr key={user.id} className="text-center">
-                            <td className="border px-4 py-2">{user.id}</td>
-                            <td className="border px-4 py-2">{user.name}</td>
-                            <td className="border px-4 py-2">{user.email}</td>
+                            <td className="border px-4 py-2 text-center">{user.id}</td>
+                            <td className="border px-4 py-2 text-center">{user.name}</td>
+                            <td className="border px-4 py-2 text-center">{user.email}</td>
                         </tr>
                     ))}
                 </tbody>
